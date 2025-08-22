d6872e82541dc988bdc2098f4df93f038a99ed89
Russel Jerome Rapi <rapi.russeljerome.gratil@gmail.com>
2025-08-23 02:46:24 +0800

    Initial commit. Pure state with no added enhancements yet.

---
+++ README.md
@@ -0,0 +1,3 @@
+# rapi_advmobprog
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/rapi_advmobprog/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rapi_advmobprog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
